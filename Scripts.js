07ab6a5d64b323a8390ef4bdf5aa231cdd775991
NathanDaniel01 <nate@thedanielfamily.com>
2021-02-09 13:43:38 -0700

    First Commit

---
+++ Scripts.js
@@ -0,0 +1,12 @@
+var timeleft = 10;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished";
+    document.getElementById("image").style.visibility = "visible";
+
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
